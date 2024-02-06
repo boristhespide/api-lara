fe80e93f0bd6b8a2e3946570ee81ebefebae280c
alexandr <boristhespide@gmail.com>
2024-02-06 23:49:11 +0800

    Initial commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return Product::all();
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|max:255',
+            'description' => 'required',
+            'price' => 'required|numeric',
+        ]);
+    }
+}
